3bcadd8894cea4073db5febb48921db8915cf63a
PerfectDay20 <zhang.stef@gmail.com>
2025-01-03 23:35:58 +0800

    theme, ssh copy

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,34 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.wrap = true
+
+-- change cursor color: https://neovim.io/doc/user/faq.html#faq
+vim.api.nvim_set_hl(0, "Cursor", { fg = "red", bg = "red" })
+
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25-Cursor,r-cr-o:hor20"
+
+-- enable ssh remote copy and paste
+-- the default conf in the help has issues when paste with 'p'
+-- so the below config is from https://github.com/neovim/neovim/discussions/28010#discussioncomment-9877494
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
---
+++ lua/plugins/rose-pine.lua
@@ -0,0 +1,11 @@
+return {
+  { "rose-pine/neovim", name = "rose-pine" },
+
+  -- Configure LazyVim to load this theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
